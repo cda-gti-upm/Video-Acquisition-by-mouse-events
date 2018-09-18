2b0e9d25d493c1473d5219d5358092d897a55f1b
raquelduenass <raquel.duenass@alumnos.upm.es>
2018-09-18 11:11:59 +0200

    Capturing data corrected

--- LeapPrueba.py
+++ LeapPrueba.py
@@ -3,6 +3,11 @@
 Created on Thu Sep 13 10:28:40 2018
 
 @author: Raquel Dueñas Suárez
+
+Capturing data from Leap Motion:
+    - Instert the gesture you are about to perform
+    - Click and keep press the left button when performing the gesture
+    - When finished, press the right button
 """
 
 import Leap, sys, os, ctypes, struct
@@ -21,24 +26,33 @@ def on_move(x, y): # Mouse moving
 
 def on_click(x, y, button, pressed): # Mouse click
     global pressing
-    print('{0}'.format(
-        'Pressed' if pressed else 'Released'))
-    if pressed:
-        pressing = 1
+    if button == mouse.Button.left:
+        print('{0}'.format(
+            'Pressed' if pressed else 'Released'))
+        if pressed:
+            pressing = 1
+        else:
+            pressing = 0
     else:
-        pressing = 0
+        return False
 
 def on_scroll(x, y, dx, dy): # Mouse scroll
-    return False
+    pass
     
 
-# CREATE DIRECTORIES
-msg = "Indique el gesto: eg = 0, 1, 2,..."
-gesto = input(msg)
+## VARIABLES
+    
+last_frame = []
 data_frames = []
 size_frames = []
 pressed_moments = []
-pressing = 0
+pressing = 0 
+
+
+## CREATE DIRECTORIES
+
+msg = "Indique el gesto: eg = 0, 1, 2,..."
+gesto = input(msg)
 
 root = os.getcwd()
 directory = root + '\\Data/gesture' + str(gesto) # gesture directory
@@ -56,7 +70,8 @@ frames_path = directory + "/savedframes.data"
 pressed_path = directory + "/pressed.txt"
     
  
-## LEAP MOTION    
+## LEAP MOTION
+    
 class LeapMotionListener(Leap.Listener):
     
     def on_init(self, controller):
@@ -72,25 +87,28 @@ class LeapMotionListener(Leap.Listener):
         print("Exited")
         
     def on_frame(self, controller):
+        global last_frame
         frame = controller.frame()
+        last_frame = frame
         data_frames.append(frame.serialize[0])
         size_frames.append(frame.serialize[1])
         pressed_moments.append(pressing)
-        print("Frame read")
         
+        """print("Frame read")
         for hand in frame.hands:
             for finger in hand.fingers:
                 if finger.type == 1:
-                    print("Index position: " + str(finger.tip_position))
+                    print("Index position: " + str(finger.tip_position))"""
                     
                     
 def main():
-    listener = LeapMotionListener()
-    controller = Leap.Controller()
     
+    controller = Leap.Controller()
+    listener = LeapMotionListener()  
     controller.add_listener(listener)
     
-    print("Roll to quit...")
+    print("Scroll to quit...")
+    
     try:
         # Mouse
         with mouse.Listener(
@@ -99,9 +117,13 @@ def main():
                 on_scroll=on_scroll) as mouseListener:
             mouseListener.join()
         sys.stdin.readline()
+        
     except KeyboardInterrupt:
         pass
+    
     finally:
+        
+        controller.remove_listener(listener) #Stop capturing data
         
         # Save frames
         with open(frames_path, "wb") as data_file:
@@ -120,7 +142,6 @@ def main():
             f.write("%s\n" % p)
         f.close()
         
-        controller.remove_listener(listener)
         
 if __name__ == "__main__":
     main()
--- read_data.py
+++ read_data.py
@@ -1,25 +1,28 @@
 # -*- coding: utf-8 -*-
 
-import Leap, struct, ctypes 
+import Leap, struct, ctypes
 
 frames = []
-with open("savedframes.data", "rb") as data_file:
-    next_block_size = data_file.read(4)
-    while next_block_size:
-        size = struct.unpack('i', next_block_size)[0]
-        data = data_file.read(size)
-        leap_byte_array = Leap.byte_array(size)
-        address = leap_byte_array.cast().__long__()
-        ctypes.memmove(address, data, size)
-        print("Block read")
 
-        frame = Leap.Frame()
-        frame.deserialize((leap_byte_array, size))
+def main():
+    controller = Leap.Controller()
+    
+    with open("savedframes.data", "rb") as data_file:
         next_block_size = data_file.read(4)
-        frames.append(frame)
-        
-        
-for hand in frames[0].hands:
-    for finger in hand.fingers:
-        if finger.type == 1:
-            print(str(finger.tip_position))
+        while next_block_size:
+            size = struct.unpack('i', next_block_size)[0]
+            data = data_file.read(size)
+            leap_byte_array = Leap.byte_array(size)
+            address = leap_byte_array.cast().__long__()
+            ctypes.memmove(address, data, size)
+            print("Block read")
+            
+            frame = Leap.Frame()
+            frame.deserialize((leap_byte_array, size))
+            next_block_size = data_file.read(4)
+            frames.append(frame)
+            
+                        
+            
+if __name__ == "__main__":
+    main()
